609f1f1dda2c97593e42ce18b31b95f3e7d6ff0b
miftahudin19 <40747706+miftahudin19@users.noreply.github.com>
2018-12-17 20:50:43 +0700

    Add files via upload

---
+++ Program Memeriksa Bilangan Terbesar dari Dua Bilangan.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+int a,b;
+
+cout<<"Menentukan bilangan terbesar dari dua buah bilangan"<<endl;
+cout<<"Masukan bilangan ke 1"<<endl;
+cin>>a;
+cout<<"Masukan bilangan ke 2"<<endl;
+cin>>b;
+
+if(a>b)
+    cout<<"Maka bilangan terbesar adalah"<<a<<endl;
+    else
+        cout<<"Maka bilangan terbesar adalah"<<b<<endl;
+    return 0;
+}
